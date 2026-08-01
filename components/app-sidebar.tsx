bedef7d4e5e2acf9097d3c76c4a76df4462f2006
puntodamar <punto.damar2@gmail.com>
2026-08-01 23:04:50 +0700

    fix: suspense

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import ScheduleList from "@/components/map/schedule/schedule-list";
 import {useScheduleStore} from "@/src/stores/schedule-store";
 import ScheduleFilter from "@/components/ui/filter/schedule-filter";
+import {Suspense} from "react";
 
 export function AppSidebar() {
     const loading = useScheduleStore((state) => state.getLoading());
@@ -25,7 +26,9 @@ export function AppSidebar() {
                     height={196}
                     className="h-auto w-40 mx-auto sm:w-25 md:w-64 lg:w-72"
                 />
-                <ScheduleFilter/>
+                <Suspense fallback={null}>
+                    <ScheduleFilter/>
+                </Suspense>
                 <h2 className="text-title text-2xl font-bold text-center dark:text-white mb-2 mt-10">
                     {loading || init ? "Mencari Jadwal..." : "Jadwal Hari Ini"}
                 </h2>
